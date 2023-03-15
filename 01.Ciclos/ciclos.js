9927a92cf48a3b16051d435fcc31935614af2a71
Jesua Luján <code.masterqro@gmail.com>
2023-03-14 20:04:20 -0600

    CICLOS

---
+++ 01.Ciclos/ciclos.js
@@ -0,0 +1,13 @@
+//* ALGUNOS EJEMPLOS DE CICLOS 
+//* EJEMPLO DE UN CICLO WHILE 
+
+var index = 0; // contador en 0
+    while( index < 11 ){ // 0 < 11 , 1 < 11 , 2 < 11 
+        //aquí nuestro código / lógica
+        console.log("VOY EN EL NÚMERO: " + index ) //concatenando
+                  // VOY EN EL NÚMERO:       1
+                  // VOY EN EL NÚMERO:       2 
+
+                  //CONTADOR
+                  index++
+    }
